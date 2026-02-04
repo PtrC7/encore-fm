d0589460ca1482a775db312890524dbc3fd2073e
PtrC7 <petechaya7@gmail.com>
2026-02-04 11:01:09 -0500

    fixed app.py to work with render

--- app.py
+++ app.py
@@ -2,17 +2,11 @@ from flask import Flask
 from config import Config
 from routes import register_routes
 
-def create_app():
-    app = Flask(__name__)
-    app.config.from_object(Config)
-    
-    # Register all routes
-    register_routes(app)
-    
-    return app
+app = Flask(__name__)
+app.config.from_object(Config)
+register_routes(app)
 
 if __name__ == "__main__":
-    app = create_app()
     app.run(
         host="0.0.0.0",
         port=app.config.get("PORT", 5000),
